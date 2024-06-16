8a89ea032b718369eb3f4e8891b6bb6a284fd669
Cliff <developerlyk@gmail.com>
2024-06-16 17:10:56 +0900

    상품 업데이트

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -37,8 +39,29 @@ public class BasicItemController {
     return "basic/item";
   }
 
-  @
+  @GetMapping("/add")
+  public String addForm() {
+    return "basic/addForm";
+  }
 
+  @PostMapping("/add")
+  public String save(@ModelAttribute("item") Item item, Model model) {
+    itemRepository.save(item);
+    return "basic/item";
+  }
+
+  @GetMapping("/{itemId}/edit")
+  public String editForm(@PathVariable Long itemId, Model model) {
+    Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "basic/editForm";
+  }
+
+  @PostMapping("/{itemId}/edit")
+  public String submitForm(@PathVariable Long itemId, @ModelAttribute Item item) {
+    itemRepository.update(itemId, item);
+    return "redirect:/basic/items/{itemId}";
+  }
 
   @PostConstruct
   public void init() {
